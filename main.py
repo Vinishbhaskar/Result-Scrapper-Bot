7926031ad9e391caed7835aa598650c99763f1c2
Vinish Bhaskar <vinish.bhaskar@outlook.com>
2023-01-19 16:31:03 +0530

    while loop to for loop speed increase

--- main.py
+++ main.py
@@ -1,59 +1,38 @@
-from Result_BOT_Final import Result_BOT
+from scrapper import Result_Bot_function, url
 
 
-def Reg_generator(Reg_Year, Reg_Branch_Code, Reg_Clg_code, Reg_Student_Id, url):
+def Reg_generator(Reg_Year, Reg_Branch_Code, Reg_Clg_code, Reg_Student_Id):
     """
-    This function wil return the all combination of registration number
-    """
-
-    """
-    Reg_Year =    [18, 19, 20, 21]
+    This function wil return the all combination of registration number:
+    Reg_Year =        [18, 19, 20, 21]
     Reg_Branch_Code = [101, 102, 103, 104]
-    Reg_Clg_code  = [130, 131, 132]
-
+    Reg_Clg_code  =   [130, 131, 132]
     """
 
-    reg = 0
-    branch = 0
-    clg = 0
-
-    i = 0
-    while reg < len(Reg_Year):
-        while clg < len(Reg_Clg_code):
-            while branch < len(Reg_Branch_Code):
-                while i < len(Reg_Student_Id):
-                    c = str(Reg_Year[reg])+str(Reg_Branch_Code[branch]
-                                               )+str(Reg_Clg_code[clg])
-                    newReg = int(c+str(Reg_Student_Id[i]).zfill(3))
-                    Result_BOT(newReg, url)
-
-                    i += 1
-                branch += 1
-                i = 0
-            branch = 0
-            clg += 1
-            i = 0
-        reg += 1
-        clg = 0
-        i = 0
-    # print(newReg)
-    # return int(newReg)
+    for reg in Reg_Year:
+        for clg in Reg_Clg_code:
+            for branch in Reg_Branch_Code:
+                for student_id in Reg_Student_Id:
+                    new_reg = f"{reg}{branch}{clg}{student_id:03d}"
+                    Result_Bot_function(int(new_reg), url)
 
 
-Reg_Year = list(range(18, 20, 1))
-Reg_Branch_Code = list(range(104, 105, 1))
-Reg_Clg_code = list(range(135, 136, 1))
-Reg_Student_Id = list(range(1, 61))
+'''The value of student_id is formatted to always have 3 digits by using the :03d syntax. This means that if student_id is less than 100, it will be padded with leading zeros to make it have 3 digits. For example, if student_id is 5, the resulting string will be 005. If student_id is 123, the resulting string will be 123.'''
 
 
-RegistrationNo = int(
-    str(Reg_Year[0])
-    + str(Reg_Branch_Code[0])
-    + str(Reg_Clg_code[0])
-    + str(Reg_Student_Id[0]).zfill(3)
-)
+if __name__ == "__main__":
+    Reg_Year = list(range(18, 19, 1))
+    Reg_Branch_Code = list(range(101, 105, 1))
+    Reg_Clg_code = list(range(113, 127, 1))
+    Reg_Student_Id = list(range(1, 61))
 
-url = f'https://......./{RegistrationNo}'
+    Reg_generator(Reg_Year, Reg_Branch_Code, Reg_Clg_code, Reg_Student_Id)
 
+    # RegistrationNo = int(
+    #     str(Reg_Year[0])
+    #     + str(Reg_Branch_Code[0])
+    #     + str(Reg_Clg_code[0])
+    #     + str(Reg_Student_Id[0]).zfill(3)
+    # )
 
-Reg_generator(Reg_Year, Reg_Branch_Code, Reg_Clg_code, Reg_Student_Id, url)
+    # url = f'https://......./{RegistrationNo}'//&RegNo={RegistrationNo}'
